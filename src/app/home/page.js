4af3e5da2cad7a1cd39914c2105a87dd489344c9
JunMango <gudwns4985@gmail.com>
2023-10-28 09:20:10 +0900

    Refactor: nav

---
+++ src/app/home/page.js
@@ -0,0 +1,24 @@
+"use client";
+import Nav from "@/view/nav";
+import "../../styles/home.css";
+export default function home() {
+  return (
+    <main className="main">
+      <div>
+        <Nav />
+      </div>
+      <div className="home">
+        <div className="home-wrap">
+          <h> main page & chat list</h>
+          <div>
+            <div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
