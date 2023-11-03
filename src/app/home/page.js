efe89aaace53d3eff1d6defae1e064289615cb96
JunMango <gudwns4985@gmail.com>
2023-11-03 21:10:13 +0900

    refactor: Nav

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,13 +1,21 @@
 "use client";
 import Nav from "@/view/nav";
 import "../../styles/home.css";
+import { useState } from "react";
 export default function home() {
+  const [isOpen, setIsOpen] = useState(true);
+  function handleNav() {
+    setIsOpen(!isOpen);
+  }
   return (
     <main className="main">
-      <div>
-        <Nav />
-      </div>
+      <div>{isOpen ? <Nav /> : null}</div>
       <div className="home">
+        <div className="nav-isOpen">
+          <button type="button" className="btn btn-primary" onClick={handleNav}>
+            {isOpen ? "close" : "open"}
+          </button>
+        </div>
         <div className="home-wrap">
           <h> main page & chat list</h>
           <div>
--- src/view/nav.js
+++ src/view/nav.js
@@ -1,44 +1,16 @@
 "use client";
-
-import styles from "../styles/nav.css";
+import "../styles/nav.css";
 import { useEffect, useState } from "react";
-
 export default function Nav() {
   // 상태관리
-  const [isOpen, setIsOpen] = useState(true);
-  const toggleNav = () => {
-    setIsOpen(!isOpen);
-  };
-
-  // useEffect(() => {
-  //   const button = document.querySelector("." + styles.button);
-  //   button.addEventListener("click", toggleNav);
-  // }, [isOpen]);
 
   return (
-    <>
-      <nav>
-        <div className="nav">
-          <div className="user-info-wrap">
-            <span> 유저 정보 칸 </span>
-            <button>{isOpen ? <span>close</span> : <span>open</span>}</button>
-          </div>
-          <div>
-            <div>
-              <span>친구 목록</span>
-            </div>
-            <div>
-              <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-              </ul>
-            </div>
-          </div>
+    <nav>
+      <div>
+        <div>
+          <div>사이드 바</div>ㅍ{" "}
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
