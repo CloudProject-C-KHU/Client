6b0c63af669517f5c7ea3925dceecd9c58fb1dd7
JunMango <gudwns4985@gmail.com>
2023-11-03 22:16:17 +0900

    add friendList

--- src/view/nav.js
+++ src/view/nav.js
@@ -3,29 +3,53 @@ import "../styles/nav.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { getFriend } from "@/const/const";
-import data from "bootstrap/js/src/dom/data";
-import { error } from "next/dist/build/output/log";
+
+/* example Data */
+export const isfriendList = [
+  {
+    id: 1,
+    name: "형준",
+  },
+  {
+    id: 2,
+    name: "재환",
+  },
+  {
+    id: 3,
+    name: "혜진",
+  },
+  {
+    id: 4,
+    name: "수환",
+  },
+  {
+    id: 5,
+    name: "성휘",
+  },
+];
 export default function Nav() {
-  const [isfriendList, setIsfriendList] = useState([]);
-  useEffect(() => {
-    axios
-      .get(getFriend)
-      .then((reponse) => {
-        let copy = [...isfriendList, ...reponse.data];
-        setIsfriendList(copy);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [isfriendList]);
+  // const [isfriendList, setIsfriendList] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get(getFriend)
+  //     .then((response) => {
+  //       let copy = [...isfriendList, ...response.data];
+  //       setIsfriendList(copy);
+  //       console.log(setIsfriendList);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
   return (
     <nav>
+      s
       <div>
         <h1>FriendList</h1>
         <div>
           <div>
-            {isfriendList.map((friend, i) => {
-              return <div>{friend[i]}</div>;
+            {isfriendList.map((a, i) => {
+              return <div>{isfriendList[i].name}</div>;
             })}
           </div>
         </div>
