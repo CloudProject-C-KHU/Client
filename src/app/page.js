55f5c3af2a59b7d2762333f31ef8187dfb3c898c
JunMango <gudwns4985@gmail.com>
2023-11-15 16:56:48 +0900

    fix page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 export default function Home() {
-  const chatURL = "/list";
+  const chatURL = "/home";
   return (
     <main className={styles.main}>
       <div>
