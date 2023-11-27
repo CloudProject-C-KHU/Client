793c4a9b23f86a08550335c006c0d6354875e205
JunMango <gudwns4985@gmail.com>
2023-11-27 19:40:49 +0900

    fix list page

--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -1,36 +1,17 @@
 "use client";
-import Nav from "@/view/nav";
-import "../../styles/home.css";
+import "../../styles/list.css";
 import { useEffect, useState } from "react";
-import TextEdit from "@/component/textEdit";
+import SideNav from "@/view/SideNav/SideNav";
+import ChatList from "@/component/ChatList";
 export default function List() {
   const [isOpen, setIsOpen] = useState(true);
-  function handleNav() {
-    setIsOpen(!isOpen);
-  }
-
   return (
-    <main className="main">
-      <div className="nav-action">
-        {isOpen ? <Nav isOpen={isOpen} /> : null}
+    <div className="page-layout">
+      <SideNav />
+      <div className="list">
+        <h1> main page & chat list</h1>
+        <ChatList />
       </div>
-      <div className="home">
-        <div className="nav-isOpen">
-          <button type="button" className="btn btn-primary" onClick={handleNav}>
-            {isOpen ? "close" : "open"}
-          </button>
-        </div>
-        <div className="home-wrap">
-          <h> main page & chat list</h>
-          <div>
-            <div>
-              <div>
-                <TextEdit />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+    </div>
   );
 }
