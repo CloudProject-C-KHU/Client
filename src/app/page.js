11057d620cde4284bd0edf33afa7601b89e02e57
JunMango <gudwns4985@gmail.com>
2023-12-18 16:45:01 +0900

    fix login useMemo

--- src/app/page.js
+++ src/app/page.js
@@ -2,48 +2,94 @@
 import styles from "./page.module.css";
 import LoginBtn from "@/component/LoginBtn/LoginBtn";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { login } from "@/api";
-
 export default function Home() {
+  // const router = useRouter();
+  // const [isCode, setIsCode] = useState(null);
+  // const [id, setId] = useState(null);
+  // const searchParams = useSearchParams();
+  // useEffect(() => {
+  //   const code = searchParams.get("code");
+  //   setIsCode(code);
+  //   if (code !== null) {
+  //     try {
+  //       console.log("fetch 실행");
+  //       FetchLogin();
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   }
+  // }, [isCode]);
+  //
+  // const FetchLogin = async () => {
+  //   try {
+  //     console.log("FetchLogin 작동 code == ", isCode); // 잘나옴
+  //     const response = await fetch(login, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ code: isCode }), //
+  //     });
+  //     if (response.ok) {
+  //       const responseData = await response.json();
+  //       const userId = responseData.id;
+  //       setId(userId);
+  //     } else {
+  //       console.error("Error from server:", response.status);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error sending data to server:", error);
+  //   }
+  // };
   const router = useRouter();
   const [isCode, setIsCode] = useState(null);
   const [id, setId] = useState(null);
   const searchParams = useSearchParams();
+
   useEffect(() => {
     const code = searchParams.get("code");
     setIsCode(code);
-    if (code !== null) {
+  }, [searchParams]);
+
+  const memoizedFetchLogin = useMemo(() => {
+    const FetchLogin = async () => {
+      try {
+        console.log("FetchLogin 작동 code == ", isCode);
+        const response = await fetch(login, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ code: isCode }),
+        });
+        if (response.ok) {
+          const responseData = await response.json();
+          const userId = responseData.id;
+          setId(userId);
+        } else {
+          console.error("Error from server:", response.status);
+        }
+      } catch (error) {
+        console.error("Error sending data to server:", error);
+      }
+    };
+
+    return FetchLogin;
+  }, [isCode]);
+
+  useEffect(() => {
+    if (isCode !== null) {
       try {
         console.log("fetch 실행");
-        FetchLogin();
+        memoizedFetchLogin(); // 여기에 괄호를 추가해야 합니다.
       } catch (e) {
         console.error(e);
       }
     }
-  }, [isCode]);
+  }, [isCode, memoizedFetchLogin]);
 
-  const FetchLogin = async () => {
-    try {
-      console.log("code == ", isCode); // 잘나옴
-      const response = await fetch(login, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ code: isCode }), //
-      });
-      if (response.ok) {
-        const responseData = await response.json();
-        const userId = responseData.id;
-        setId(userId);
-      } else {
-        console.error("Error from server:", response.status);
-      }
-    } catch (error) {
-      console.error("Error sending data to server:", error);
-    }
-  };
   return (
     <main className={styles.main}>
       <div>
