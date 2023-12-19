3411502830f062fef9dddab6b074a9b355a81673
JunMango <gudwns4985@gmail.com>
2023-12-20 00:07:52 +0900

    add api

--- src/api.jsx
+++ src/api.jsx
@@ -6,5 +6,7 @@ export const login = baseUrl + "/auth";
 export const noteList = baseUrl + "/note/list";
 // export const noteList = "/note/list";
 export const getFriend = baseUrl + "/friend/list";
+// export const KAKAO_LOGIN =
+//   "https://kauth.kakao.com/oauth/authorize?client_id=c79a3544b3466643b7709be0f727f138&redirect_uri=http://localhost:3000&response_type=code";
 export const KAKAO_LOGIN =
-  "https://kauth.kakao.com/oauth/authorize?client_id=c79a3544b3466643b7709be0f727f138&redirect_uri=http://localhost:3000&response_type=code";
+  "https://kauth.kakao.com/oauth/authorize?client_id=c79a3544b3466643b7709be0f727f138&redirect_uri=https://client-gamma-bay.vercel.app/&response_type=code";
